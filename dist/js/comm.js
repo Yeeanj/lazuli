3dae946aa6540b37e410793f42a15ee927be1e6d
belluna <bellashu1@naver.com>
2023-08-04 18:06:50 +0900

    123

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -1,6 +1,15 @@
-var swiper = new Swiper(".mySwiper", {
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+// var swiper = new Swiper(".mySwiper", {
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+
+
+$(function () {
+  const deplink1 = $(".link-depth1");
+  const linkBtn = $(".link-main");
+  linkBtn.on("click", function () {
+    deplink1.addClass("active");
+  });
 });
